7fb2386e46f455b023dd86258069448bbd16c39c
Ritesh <ritesh.adwani.18@gmail.com>
2026-01-11 23:34:20 +0530

    Revert "fix: use DATABASE_PATH env variable for SQLite in production"
    This reverts commit daa2cab37ef6b7ca58f563017674fa7817a088d5.

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,17 +1,9 @@
 import knex from 'knex';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Use DATABASE_PATH env variable in production, or default to local path
-const dbPath = process.env.DATABASE_PATH || join(__dirname, '../data/db.sqlite3');
 
 export const connection = knex({
   client: 'better-sqlite3',
   connection: {
-    filename: dbPath,
+    filename: './src/data/db.sqlite3',
   },
   useNullAsDefault: true,
 });
